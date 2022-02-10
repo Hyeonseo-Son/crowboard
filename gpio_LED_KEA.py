b619fc66247f0f3f3213587d877abf08aaf67b42
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-02-10 16:10:43 +0900

    Update gpio_LED_KEA.py

--- gpio_LED_KEA.py
+++ gpio_LED_KEA.py
@@ -7,12 +7,12 @@ pin3 = 48
 
 
 def led_on(pin):
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, True)
 
 def led_off(pin):
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
     GPIO.cleanup(pin)
 
