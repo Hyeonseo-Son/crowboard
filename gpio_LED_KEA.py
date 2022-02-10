b9861b92d8b8b0b4bed1b05566be6afc9846c596
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-02-10 14:44:42 +0900

    Add files via upload

---
+++ gpio_LED_KEA.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+
+pin1 = 1
+pin2 = 7
+pin3 = 9
+
+
+
+def led_on(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+
+    GPIO.output(pin, True)
+
+def led_off(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+
+    GPIO.cleanup(pin)
+
+led_on(pin1)
+print('pin1 ON')
+time.sleep(0.5)
+led_off(pin1)
+print('pin1 OFF')
+time.sleep(0.5)
+
+led_on(pin2)
+print('pin2 ON')
+time.sleep(0.5)
+led_off(pin2)
+print('pin2 OFF')
+time.sleep(0.5)
+
+led_on(pin3)
+print('pin3 ON')
+time.sleep(0.5)
+led_off(pin3)
+print('pin3 OFF')
+time.sleep(0.5)
