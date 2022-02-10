bb773340679fdbba07bd2a0cce23baac23abb2f7
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-02-10 14:38:41 +0900

    Update and rename gpio2.py to gpio_KEA.py

--- gpio_KEA.py
+++ gpio_KEA.py
@@ -3,16 +3,16 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.IN)         #Read output from PIR motion sensor
-GPIO.setup(17, GPIO.OUT)         #LED output pin
+GPIO.setup(13, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setup(12, GPIO.OUT)         #LED output pin
 
 while True:
-    i=GPIO.input(16)
+    i=GPIO.input(13)
     if i==0:                 #When output from motion sensor is LOW
         print ("No intruders",i)
-        GPIO.output(17, 0)  #Turn OFF LED
+        GPIO.output(12, 0)  #Turn OFF LED
         time.sleep(0.1)
     elif i==1:               #When output from motion sensor is HIGH
         print ("Intruder detected",i)
-        GPIO.output(17, 1)  #Turn ON LED
+        GPIO.output(12, 1)  #Turn ON LED
         time.sleep(0.1)
