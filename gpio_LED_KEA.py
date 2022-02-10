5c4e983e46900f532a0d8cc03787080eb7fa6271
Hyeonseo <54432870+hyeonseo0128@users.noreply.github.com>
2022-02-10 15:12:38 +0900

    Update gpio_LED_KEA.py

--- gpio_LED_KEA.py
+++ gpio_LED_KEA.py
@@ -1,41 +1,16 @@
 import Jetson.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BOARD)
+channel = 1
 
-pin1 = 1
-pin2 = 7
-pin3 = 9
+GPIO.setup(channel, GPIO.OUT)
 
+while True:
+   GPIO.output(channel, GPIO.HIGH)
+   
+   time.sleep(0.5)
+   GPIO.output(channel, GPIO.LOW)
 
+   time.sleep(0.5)
 
-def led_on(pin):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin, GPIO.OUT)
-
-    GPIO.output(pin, True)
-
-def led_off(pin):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin, GPIO.OUT)
-
-    GPIO.cleanup(pin)
-
-led_on(pin1)
-print('pin1 ON')
-time.sleep(0.5)
-led_off(pin1)
-print('pin1 OFF')
-time.sleep(0.5)
-
-led_on(pin2)
-print('pin2 ON')
-time.sleep(0.5)
-led_off(pin2)
-print('pin2 OFF')
-time.sleep(0.5)
-
-led_on(pin3)
-print('pin3 ON')
-time.sleep(0.5)
-led_off(pin3)
-print('pin3 OFF')
-time.sleep(0.5)
+GPIO.cleanup()
